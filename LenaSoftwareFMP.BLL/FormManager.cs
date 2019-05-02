4e06ae4c934caad9c3fde7a6ca56e249753b8381
DilnurSeda <dlnrsdngn@gmail.com>
2019-05-03 01:44:58 +0300

    Add files via upload

---
+++ LenaSoftwareFMP.BLL/FormManager.cs
@@ -0,0 +1,47 @@
+﻿using LenaSoftwareFMP.DLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LenaSoftwareFMP.BLL
+{
+    public class FormManager
+    {
+        LenaSoftwareDBEntities db = new LenaSoftwareDBEntities();
+
+        public List<forms> FormList()
+        {
+            return db.forms.ToList();
+        }
+
+        public string FormInsert(string name,string description,DateTime? createdAt,int? createdBy)
+        {
+            try
+            {
+                forms insert = new forms();
+                insert.name = name;
+                insert.description = description;
+                insert.createdAt = createdAt;
+                insert.createdBy = createdBy;
+
+                db.forms.Add(insert);
+                int result = db.SaveChanges();
+                if (result > 0)
+                {
+                    return "Kayıt başarılı";
+                }
+                else
+                {
+                    return "Kayıt yapılamadı";
+                }
+            }
+            catch (Exception)
+            {
+
+                return "Hata oluştu";
+            }
+        }
+    }
+}
---
+++ LenaSoftwareFMP.BLL/LoginManager.cs
@@ -0,0 +1,27 @@
+﻿using LenaSoftwareFMP.DLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LenaSoftwareFMP.BLL
+{
+    public class LoginManager
+    {
+        LenaSoftwareDBEntities db = new LenaSoftwareDBEntities();
+        /// <summary>
+        /// --veritabanindaki verilerle eşleştirip ilk bulduğunu ya da varsayılanı getirir.
+        /// --varsa true doner.
+        /// </summary>
+        /// <param name="kullaniciAdi"></param>
+        /// <param name="sifre"></param>
+        /// <returns></returns>
+        public Users Login(string username, string password)
+        {
+            var giris = db.Users.Where(k => k.username == username && k.password == password).FirstOrDefault();
+
+            return giris;
+        }
+    }
+}
