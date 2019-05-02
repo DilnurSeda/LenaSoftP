9fc635d6fce8d3e848df50770bba551295f8c53c
DilnurSeda <dlnrsdngn@gmail.com>
2019-05-03 01:52:53 +0300

    Add files via upload

---
+++ LenaSoftwareFMPV1/Controllers/FormController.cs
@@ -0,0 +1,45 @@
+﻿using LenaSoftwareFMP.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LenaSoftwareFMPV1.Controllers
+{
+    public class FormController : Controller
+    {
+        FormManager formManager = new FormManager();
+        // GET: Form
+        public ActionResult FormIndex()
+        {
+            
+            if (Session["ActiveUser"] == null)
+            {
+                return RedirectToAction("LoginIndex", "Login");
+            }
+            return View();
+        }
+        
+        public ActionResult FormDisplayIndex()
+        {
+            if (Session["ActiveUser"] == null)
+            {
+                return RedirectToAction("LoginIndex", "Login");
+            }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult FormIndex(string name, string description, DateTime? createdAt, int? createdBy)
+        {
+            createdBy = Convert.ToInt32(Session["UserID"].ToString());
+            formManager.FormInsert(name, description, Convert.ToDateTime(DateTime.Now.ToShortDateString()), (int)createdBy);
+            return View();
+        }
+        [HttpPost]
+        public ActionResult FormDisplayIndex(int formId)
+        {            
+            return View();
+        }
+    }
+}
---
+++ LenaSoftwareFMPV1/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using LenaSoftwareFMP.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LenaSoftwareFMPV1.Controllers
+{
+    public class LoginController : Controller
+    {
+        LoginManager loginManager = new LoginManager();
+        // GET: Login
+        public ActionResult LoginIndex()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult LoginIndex(string username, string password)
+        {
+            var loginGiris = loginManager.Login(username, password);
+            if (loginGiris!=null)
+            {
+                //user id send to createdBY
+                Session.Add("UserID",loginGiris.ID);
+                Session.Add("ActiveUser",username);
+                return RedirectToAction("FormIndex", "Form");
+            }
+            else
+            {
+                ViewBag.mesaj = "Kullanıcı Adı veya Şifre Hatalı!";
+            }
+            return View();
+        }
+    }
+}
